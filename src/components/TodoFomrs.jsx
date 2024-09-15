2edef5d07556f56a213216ddd7f4dd6380a4e886
Pedro H <ph159.com@gmail.com>
2024-09-15 17:13:14 -0300

    enviando projero  para deploy 2

---
+++ src/components/TodoFomrs.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const TodoFomrs = ({addTodo}) => {
+  const [value, setValue] = useState('')
+  const [category, setCategory] = useState('')
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!value || !category) return
+    addTodo(value, category)
+    setValue('')
+    setCategory('')
+  }
+
+  return (
+    <div className='todo-form'>
+        <h2>Criar Tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Digite o titulo' value={value} onChange={(e) => setValue(e.target.value)}/>
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Selecione uma caregoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type='submit'>Criar tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoFomrs
